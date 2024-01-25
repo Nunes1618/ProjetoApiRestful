ebb0fb68b811b9bd43456b98bcecba8f532be4de
Nunes1618 <wesley.nunes75@gmail.com>
2024-01-25 17:01:49 -0300

    Finalização do projeto API restful utilizando o MongoDb

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+const conn = require("./db/conn");
+
+conn();
+
+// Routes
+const routes = require("./routes/router");
+
+app.use("/api", routes);
+
+app.listen(3000, function () {
+    console.log("Servidor online!");
+});
+
+// nome usuário banco wesleynunes75
+// senha banco iAELmXBClpObGOCU
